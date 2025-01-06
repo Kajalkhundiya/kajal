2d5a5b3a551e3801553b479940801059e785b508
Kajalkhundiya <kajalnlk035@gmail.com>
2025-01-07 00:59:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#hide-btn').click(function(){
+        $('.form-popup').show()
+    })
+    $('.Close').click(function(){
+        $('.form-popup').hide()
+    })
+})
+
+
+
